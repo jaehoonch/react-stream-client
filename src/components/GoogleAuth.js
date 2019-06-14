1fe7b241a253d5a10a08102ef098ed3fd10b8575
cdh <jaehoon92@gmail.com>
2019-06-15 04:11:04 +0900

    added React Router

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+    componentDidMount(){
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '537215415700-j53kna3o6mrpd0fkfjjln2hugjb8hp21.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
+        });
+    }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    renderAuthButton(){
+        if(this.state.isSignedIn === null){
+            return <div>I don't know if we are signed in</div>;
+        }else if (this.state.isSignedIn){
+            return <div>I am signed in!</div>
+        }else{
+            return <div>I am not signed in</div>
+        }
+    }
+
+    render(){
+        return <div>{this.renderAuthButton()}</div>;
+    }
+}
+
+export default GoogleAuth;
